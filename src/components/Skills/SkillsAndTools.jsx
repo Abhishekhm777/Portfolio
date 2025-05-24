c25e8259bf7ffe183e4511a921585bec3a7e03ce
Abhishek HM <abhishek.hm@gameskraft.com>
2025-05-24 22:23:23 +0530

    Folder rename to standard

--- src/components/Skills/SkillsAndTools.jsx
+++ src/components/Skills/SkillsAndTools.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import "./SkillsAndTools.css";
 import GridView from "./GridView";
-import { FRAMEWORKS, LANGUAGES, TOOLS } from "../../constants/const";
+import { FRAMEWORKS, LANGUAGES } from "../../constants/const";
 
 const SkillsAndTools = () => {
-  const Content = ({ icon, text }) => {
-    return (
-      <div className="component">
-        <img src={icon} alt={text} className="icon" />
-        <p>{text}</p>
-      </div>
-    );
-  };
-
   return (
     <div className="container">
       <div className="header">
